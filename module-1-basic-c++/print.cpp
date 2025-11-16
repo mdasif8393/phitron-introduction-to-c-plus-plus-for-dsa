7880148a295cf989e610bfead7309f486a955e32
mdasif8393 <md.asif.8393@gmail.com>
2025-11-16 11:25:44 +0600

    print in c++

--- module-1-basic-c++/print.cpp
+++ module-1-basic-c++/print.cpp
@@ -1,14 +1,27 @@
+// #include<iostream>
+
+// int main(){
+
+//     int x = 10;
+//     char c = 'A';
+//     double d = 12.33;
+
+//     std :: cout << x << std::endl << c << std::endl << d << " " << std:: endl; 
+
+//     std::cout << "My favourite number is: " << x << std::endl;
+
+//     return 0;
+// }
+
 #include<iostream>
-
 int main(){
+    int a = 10;
+    char b = 'A';
+    double c = 12.22;
 
-    int x = 10;
-    char c = 'A';
-    double d = 12.33;
+    std::cout << a << std::endl << b << std:: endl << c << std :: endl;
 
-    std :: cout << x << std::endl << c << std::endl << d << " " << std:: endl; 
-
-    std::cout << "My favourite number is: " << x << std::endl;
+    std::cout << "My favourite number is: " << c;
 
     return 0;
 }
