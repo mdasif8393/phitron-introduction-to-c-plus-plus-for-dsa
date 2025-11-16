ab7cf820a39a8331c7bb64f7a2921ba34cf19b8d
mdasif8393 <md.asif.8393@gmail.com>
2025-11-16 11:50:38 +0600

    input in c++

---
+++ module-1-basic-c++/input.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    char c;
+    double d;
+
+    cin >> a >> c >> d;
+    cout << a << endl << c << endl << d << endl << (int)c << endl << (char)a;
+
+    return 0;
+}
