902221fb37c020db44745e2f380320b476adc5b1
mdasif8393 <md.asif.8393@gmail.com>
2025-11-28 16:38:54 +0600

    string constructor

---
+++ module-6-more-about-class-and-string/string_constructor.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // 1st way
+    // string s("Hello");
+
+    // 2nd way
+    // string s("Hello", 4); // output: Hell
+
+    // 3rd way
+    // string s = "Hello World";
+    // string t(s, 4); // output: o World
+
+    // 4th way
+    string s(5, 'A'); // Output: AAAAA
+
+    cout << s << endl;
+
+    return 0;
+}
