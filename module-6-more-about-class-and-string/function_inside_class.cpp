645671756292c65816e60d71bd424d84dcae2a47
mdasif8393 <md.asif.8393@gmail.com>
2025-11-28 19:16:25 +0600

    function inside class

---
+++ module-6-more-about-class-and-string/function_inside_class.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    string name;
+    int roll;
+    int math;
+    int english;
+
+    Student(string name, int roll, int math, int english)
+    {
+        this->name = name;
+        this->roll = roll;
+        this->math = math;
+        this->english = english;
+    };
+
+    void total()
+    {
+        cout << "Total marks of " << name << " = " << math + english << endl;
+    }
+};
+
+int main()
+{
+
+    Student sakib("Sakib Ahmed", 23, 54, 65);
+    sakib.total();
+
+    Student rakib("Rakib Zaman", 21, 65, 76);
+    rakib.total();
+    return 0;
+}
