757ba419796dde265414e9fa2c41910cbc8f6b97
mdasif8393 <md.asif.8393@gmail.com>
2025-11-30 09:41:08 +0600

    https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/M

---
+++ K_I_Love_strings.cpp
@@ -0,0 +1,16 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/K
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s1;
+    cin >> s1;
+    string s2;
+    cin >> s2;
+
+    s1.push_back('A');
+    cout << s1;
+
+    return 0;
+}
