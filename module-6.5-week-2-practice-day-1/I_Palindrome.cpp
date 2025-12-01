e694a92314ee2ff4abe6fbdf56c4bef5ffbb22cb
mdasif8393 <md.asif.8393@gmail.com>
2025-12-01 17:13:15 +0600

    solce codeforce problem: https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/I

---
+++ module-6.5-week-2-practice-day-1/I_Palindrome.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    int i = 0;
+    int j = s.size() - 1;
+
+    int palindrome = 1;
+
+    while (i < j)
+    {
+        if (s[i] != s[j])
+        {
+            palindrome = 0;
+        }
+
+        i++;
+        j--;
+    }
+
+    palindrome == 1 ? cout << "YES" : cout << "NO";
+
+    return 0;
+}
