e657f64a59df3f75880cc4485a301cdf42163ca3
mdasif8393 <md.asif.8393@gmail.com>
2025-12-01 18:19:03 +0600

    solce codeforce problem: https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/V

---
+++ module-6.5-week-2-practice-day-1/V_Replace_Word.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    int n = s.size();
+
+    string result;
+
+    for (int i = 0; i < n;)
+    {
+        if (i + 4 < n && s[i] == 'E' && s[i + 1] == 'G' && s[i + 2] == 'Y' && s[i + 3] == 'P' && s[i + 4] == 'T')
+        {
+            result += ' ';
+            i += 5;
+        }
+        else
+        {
+            result += s[i];
+            i++;
+        }
+    }
+
+    cout << result;
+    return 0;
+}
