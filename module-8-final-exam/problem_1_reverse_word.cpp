7708454ee283f61a2df1438c82688424d3ffdcb0
mdasif8393 <md.asif.8393@gmail.com>
2025-12-02 20:07:05 +0600

    solve problem 5

--- module-8-final-exam/problem_1_reverse_word.cpp
+++ module-8-final-exam/problem_1_reverse_word.cpp
@@ -5,12 +5,52 @@ using namespace std;
 
 int main()
 {
-    string s, x;
-    cin >> s >> x;
+    int x;
+    cin >> x;
 
-    int n = s.size();
-    string ans;
+    for (int i = 0; i < x; i++)
+    {
+        string s, w;
+        cin >> s >> w;
 
-    cout << n;
+        int string_size = s.size();
+        int word_size = w.size();
+
+        string result;
+
+        for (int i = 0; i < string_size;)
+        {
+            bool is_match = true;
+
+            if ((i + word_size - 1) < string_size)
+            {
+                for (int j = 0; j < word_size; j++)
+                {
+                    if (s[i + j] != w[j])
+                    {
+                        is_match = false;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                is_match = false;
+            }
+
+            if (is_match)
+            {
+                result += '#';
+                i += word_size;
+            }
+            else
+            {
+                result += s[i];
+                i++;
+            }
+        }
+        cout << result << endl;
+    }
+
     return 0;
 }
---
+++ module-8-final-exam/tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+
+            is_match = false;
+        }
