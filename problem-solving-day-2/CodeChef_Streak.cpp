0d91943653612b7509f8d11ed4f42ee69fec2e0f
mdasif8393 <md.asif.8393@gmail.com>
2025-12-12 15:48:57 +0600

    https://www.codechef.com/problems/CS2023_STK

---
+++ problem-solving-day-2/CodeChef_Streak.cpp
@@ -0,0 +1,30 @@
+// https://www.codechef.com/problems/CS2023_STK
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int b[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> b[i];
+    }
+
+    int a_count = 0;
+    int b_count = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        }
+
+    return 0;
+}
